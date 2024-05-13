5651fcc35aa8f241285d5c38eb87965d017883a0
Kaleem Ullah <39130173+ukaleem@users.noreply.github.com>
2024-05-13 20:35:00 +0500

    implement modal

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.get("/", function (req, res) {
-  res.send({
-    Message: "New data passed",
-    Error: false,
-  });
-});
-app.get("/home", function (req, res) {
-  res.send({
-    Message: "New data home",
-    Error: false,
-  });
+const port = 3000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+
+app.get("/", (req, res) => {
+  console.log("its get");
+  res.send("Hello world");
 });
 
-app.listen(2000, ()=>{
-    console.log("Server is running on port 2000");
-})
+app.post("api/product", (req, res) => {
+  console.log('api hit');
+  // res.send(req.body);
+});
+
+// Run the Server
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
+
+// Connect to database
+mongoose
+  .connect(
+    "mongodb+srv://ukaleem540:ukaleem540@crud-apis.3svbori.mongodb.net/?retryWrites=true&w=majority&appName=Crud-apis"
+  )
+  .then((res) => {
+    console.log("Database is connected");
+
+  })
+  .catch((err) => {
+    console.log("Database is not connected");
+  });
---
+++ models/products.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "name is required"],
+        },
+        price: {
+            type: Number,
+            required: [true, 'price is required'],
+        },
+        quantity: {
+            type: Number,
+            required: [true, 'Quantity is required']
+        }
+    },
+    {
+            timestamps: true
+    }
+
+);
+
+const product = mongoose.model('Products',productSchema);
+module.exports({
+    product
+});
