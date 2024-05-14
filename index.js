d48c105efd20d4c340a122fec48b6044a67bdbca
Kaleem Ullah <39130173+ukaleem@users.noreply.github.com>
2024-05-14 14:17:24 +0500

    Final updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const productRoutes = require('./routes/products.route');
+const { environment } = require("./env.js");
 
 
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+
   res.send("Hello world");
 });
 
@@ -20,17 +22,18 @@ app.use('/api/products',productRoutes)
 
 // Connect to database
 mongoose
-  .connect(
-    "mongodb+srv://ukaleem540:ukaleem540@crud-apis.3svbori.mongodb.net/?retryWrites=true&w=majority&appName=Crud-apis"
+.connect(
+  `mongodb+srv://ukaleem540:${environment.pw}@crud-apis.3svbori.mongodb.net/?retryWrites=true&w=majority&appName=Crud-apis`
   )
   .then((res) => {
     console.log("Database is connected");
     // Run the Server
     app.listen(port, () => {
+      console.log("ENV YOUR MONGODB PASSWORD:", environment.pw);
       console.log("Server is running on port " + port);
     });
-
   })
-  .catch((err) => {
+ .catch((err) => {
     console.log("Database is not connected");
+    // console.error("Database connection error:", err);
   });
